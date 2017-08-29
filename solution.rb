6deda92bc92f49cc03a68219b522d8fd3fa8ed80
Your Name <you@example.com>
2017-08-29 17:44:41 -0500

    Crea los mensajes de bienvenida de la aplicacion

--- solution.rb
+++ solution.rb
@@ -1,1 +1,15 @@
-puts "hola"
+puts "Bienvenido a reto 5, Para jugar, solo ingresa el termino correcto para cada una de las definiciones, Listo? Vamos!"
+puts "Para empezar a jugar digita 1"
+puts "Para salir digita 0"
+
+STDOUT.flush  
+text = gets.chomp.to_i
+if text == 1
+	if File.file?('definiciones.txt')
+		f = File.open('definiciones.txt').each_line do |line|
+        	p line
+    	end	
+	end
+else
+	puts "Es una lastima :("
+end
