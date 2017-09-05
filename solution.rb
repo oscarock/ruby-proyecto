aa0fd6ba9b1bffe97e8f73329df228292660fee7
Your Name <you@example.com>
2017-09-05 11:23:21 -0500

    ajustes ejercicio

--- solution.rb
+++ solution.rb
@@ -2,24 +2,22 @@ puts "Bienvenido a reto 5, Para jugar, solo ingresa el termino correcto para cad
 puts "Para empezar a jugar digita 1"
 puts "Para salir digita 0"
 
-STDOUT.flush  
 text = gets.chomp.to_i
 if text == 1
 	system 'cls'
-	preguntas = File.readlines('definiciones.txt')
+	questions = File.readlines('definiciones.txt')
 
-	preguntas.each_slice(3) do |pregunta,respuesta|
+	questions.each_slice(3) do |question,answer|
 		loop do
-	    puts pregunta
-	    given_answer = gets.chomp
+	    puts question
+	    entry_answer = gets.chomp
 
-	    if given_answer != respuesta.strip
+	    if entry_answer != answer.strip
 	    	puts "Incorrecto!!!, Trata de nuevo."
 	    else
 	    	puts "Correcto!!!."	
 	    end
-	    # given_answer != respuesta.strip ? "Incorrecto!!!, Trata de nuevo." : "Correcto!!!, Continua."
-	    break if given_answer == respuesta.strip
+	    break if entry_answer == answer.strip
 	  end
 	end
 else
