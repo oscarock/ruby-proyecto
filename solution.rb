b2339be30c0e6bd6453edd78d7c34954d3351114
Your Name <you@example.com>
2017-09-05 10:09:58 -0500

    Crea la funcionalidad del ejercicio

--- solution.rb
+++ solution.rb
@@ -1,32 +1,30 @@
-# puts "Bienvenido a reto 5, Para jugar, solo ingresa el termino correcto para cada una de las definiciones, Listo? Vamos!"
-# puts "Para empezar a jugar digita 1"
-# puts "Para salir digita 0"
+puts "Bienvenido a reto 5, Para jugar, solo ingresa el termino correcto para cada una de las definiciones, Listo? Vamos!"
+puts "Para empezar a jugar digita 1"
+puts "Para salir digita 0"
 
-# STDOUT.flush  
-# text = gets.chomp.to_i
-# if text == 1
-	# if File.file?('definiciones.txt')
-	# 	f = File.open('definiciones.txt').each_line do |line|
- #        	p line
- #        	# STDOUT.flush  
-	# 				# text = gets.chomp
+STDOUT.flush  
+text = gets.chomp.to_i
+if text == 1
+	system 'cls'
+	preguntas = File.readlines('definiciones.txt')
 
- #    	end	
-	# end
-	preguntas = File.readlines("definiciones.txt").grep(/(Definicion)/)
-	respuestas = File.readlines("definiciones.txt").grep(/(Respuesta)/)
-	preguntas.each.with_index do |value,index| 
-		puts preguntas[index]
-		STDOUT.flush  
-		text = gets.chomp
-		# puts respuestas[index]
+	preguntas.each_slice(3) do |pregunta,respuesta|
+		loop do
+	    puts pregunta
+	    given_answer = gets.chomp
+
+	    if given_answer != respuesta.strip
+	    	puts "Incorrecto!!!, Trata de nuevo."
+	    else
+	    	puts "Correcto!!!."	
+	    end
+	    # given_answer != respuesta.strip ? "Incorrecto!!!, Trata de nuevo." : "Correcto!!!, Continua."
+	    break if given_answer == respuesta.strip
+	  end
 	end
-	# file='definiciones.txt'
-	# File.readlines(file).each do |line|
-	#   puts line
-	#   STDOUT.flush  
-	# 	text = gets.chomp.to_i
-	# end
-# else
-# 	puts "Es una lastima :("
-# end
+else
+	system 'cls'
+	puts "Es una lastima :("
+end
+
+
