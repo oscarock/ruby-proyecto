fb657b47943116fa0e4ae591c1437d0d19c5f368
Your Name <you@example.com>
2017-09-06 17:37:33 -0500

    ajustes al controller mensaje de bienvenida

--- Controller.rb
+++ Controller.rb
@@ -1,3 +1,17 @@
+require_relative "Model.rb"
+require_relative "View.rb"
+
 class Controller
-	
+
+	def initialize
+		@model = Model.new	
+	end
+
+	def run
+		messages_welcome = View.message_welcome
+		text = gets.chomp.to_i
+	end
 end
+
+project =  Controller.new
+project.run
--- View.rb
+++ View.rb
@@ -1,3 +1,8 @@
 class View
-	
+	def self.message_welcome
+		puts "Bienvenido a reto 5, Para jugar, solo ingresa el termino correcto para cada una de las definiciones, Listo? Vamos!"
+		puts "Para empezar a jugar digita 1"
+		puts "Para salir digita 0"		
+	end
 end
+
